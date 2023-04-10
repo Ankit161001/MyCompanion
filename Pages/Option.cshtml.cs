20b076a2b97af222279bcc38a4509566e9a988f0
Ankit Rana <ankit.kbl@gmail.com>
2023-04-10 21:01:18 +0530

    Add project files.

---
+++ Pages/Option.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCompanion.Pages
+{
+    public class OptionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
